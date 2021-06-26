8caeeb846e5ad45382539db31831543f3465b2e0
azzerative <salehzaidan53@gmail.com>
2021-06-26 18:32:33 +0700

    Configure formatter and vendor styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
-import '../styles/globals.css'
-import '../styles/main.scss'
+import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
