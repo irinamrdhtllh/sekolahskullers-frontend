bddb3e83cfbb019154bde2f7b330d1a62852d86f
azzerative <salehzaidan53@gmail.com>
2021-07-01 11:43:49 +0700

    Update Button hover cursor

--- components/Button.js
+++ components/Button.js
@@ -6,13 +6,15 @@ import styles from '../styles/components/Button.module.scss';
 
 const Button = React.forwardRef(({ onClick, href, name }, ref) => {
   return (
-    <div className={styles.button}>
-      <Link href={href} passHref>
-        <a className={styles.link} onClick={onClick} ref={ref}>
-          {name}
-        </a>
-      </Link>
-    </div>
+    <Link href={href} passHref>
+      <a
+        className={styles.button}
+        onClick={onClick}
+        ref={ref}
+      >
+        {name}
+      </a>
+    </Link>
   );
 });
 
