346e08bc7bd3e7683314ad7644b6a8a611302a34
irinamrdhtllh <irinamardhatillah@gmail.com>
2021-07-05 17:37:13 +0700

    Add dropdown menu to header

--- layout/Header.js
+++ layout/Header.js
@@ -15,22 +15,25 @@ export default function Header() {
           <Link href="/students">Students</Link>
           <Link href="/groups">Groups</Link>
           <Link href="/class-year">Class Year</Link>
-          {isAuthenticated ? (
-            <>
-              <Link href="/profile">Profile</Link>
-              <Link href="/profile-group">Group Profile</Link>
-            </>
-          ) : null}
         </div>
+
         {!isAuthenticated ? (
           <div className="Button">
             <Button href="/auth/login" name="Login" />
             <Button href="/auth/register" name="Register" />
           </div>
         ) : null}
+        
         {isAuthenticated ? (
           <div className="Button">
-            <Button href="/auth/logout" name="Logout"/>
+            <div className="dropdown">
+              <Button href="/#" name="User" />
+              <div className="dropdown-content">
+                <Link href="/profile">Profile</Link>
+                <Link href="/profile-group">Group Profile</Link>
+              </div>
+            </div>
+            <Button href="/auth/logout" name="Logout" />
           </div>
         ) : null}
       </div>
