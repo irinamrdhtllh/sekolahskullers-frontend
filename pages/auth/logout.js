4569f2b4db525fd8544a0c69f0e241dfc762f8dc
azzerative <salehzaidan53@gmail.com>
2021-07-12 15:09:28 +0700

    Change logout button behaviour

--- pages/auth/logout.js
+++ pages/auth/logout.js
@@ -1,26 +1,17 @@
+import { useEffect } from 'react';
+
 import { useRouter } from 'next/router';
 
 import { useAuth } from '../../hooks/useAuth';
-import Layout from '../../layout/Layout';
 
 export default function Login() {
-  const { isAuthenticated, logout } = useAuth();
+  const { logout } = useAuth();
   const router = useRouter();
 
-  const onLogout = async () => {
+  useEffect(() => {
     logout();
     router.push('/');
-  };
+  }, []);
 
-  return (
-    <>
-      <Layout>
-      {isAuthenticated ? (
-        <button onClick={() => onLogout()}>Logout</button>
-      ) : (
-        <p>You have been logged out.</p>
-      )}
-      </Layout>
-    </>
-  );
+  return <></>;
 }
