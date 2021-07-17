1191c4d24555bb6806e90d0befb446c77e0f9e15
irinamrdhtllh <irinamardhatillah@gmail.com>
2021-07-17 18:13:28 +0700

    Update Student Item and Class Item component styling

--- components/StudentItem.js
+++ components/StudentItem.js
@@ -2,10 +2,21 @@ import Image from 'next/image';
 
 import styles from '../styles/components/StudentItem.module.scss';
 
-export default function StudentItem({ src, student }) {
+export default function StudentItem({
+  src,
+  student,
+  width,
+  height,
+  leaderboard,
+  group,
+}) {
   return (
-    <div className={styles.card}>
-      <Image className={styles.img} src={src} width="130" height="130" alt="image" />
+    <div
+      className={`${styles.card} ${leaderboard && styles.leaderboard} ${
+        group && styles.group
+      }`}
+    >
+      <Image src={src} width={width} height={height} alt="image" />
       <div className={styles.student}>
         <p>{student}</p>
       </div>
