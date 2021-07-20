cd2396acd47ceb35ce0622d3cb5648b1daf728e3
irinamrdhtllh <irinamardhatillah@gmail.com>
2021-07-20 14:56:01 +0700

    Add all time and weekly leaderboard tabs to groups page

--- pages/groups.js
+++ pages/groups.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import axios from 'axios';
 import Image from 'next/image';
 
@@ -9,28 +11,51 @@ import igrave from '../public/svg/igrave-black.svg';
 import styles from '../styles/pages/Groups.module.scss';
 
 export default function Groups({ groups }) {
+  const [show, setShow] = useState(true);
+
   return (
     <Layout title="Dashboard Kelas">
       <div className={styles.container}>
         <div className={styles.leaderboard}>
           <h1>Leaderboard</h1>
+          <div className={styles.tabs}>
+            <button onClick={() => setShow(true)}>All Time</button>
+            <button onClick={() => setShow(false)}>Weekly</button>
+          </div>
           <div className={styles.ship}>
             <Image src={acute} width="400" height="250" alt="svg" />
             <Image src={igrave} width="400" height="250" alt="svg" />
           </div>
         </div>
-        <div className={styles.classItem}>
-          {groups.map((group, index) => (
-            <ClassItem
-              key={index}
-              status={group}
-              number={index + 1}
-              class_logo={image}
-              health="100%"
-              exp="70%"
-            />
-          ))}
-        </div>
+        {show && (
+          <div className={styles.classItem}>
+            {groups.map((group, index) => (
+              <ClassItem
+                key={index}
+                status={group}
+                number={index + 1}
+                class_logo={image}
+                health="100%"
+                exp="70%"
+              />
+            ))}
+          </div>
+        )}
+        {!show && (
+          <div className={styles.classItem}>
+            <p>Weekly Leaderboard</p>
+            {groups.map((group, index) => (
+              <ClassItem
+                key={index}
+                status={group}
+                number={index + 1}
+                class_logo={image}
+                health="100%"
+                exp="70%"
+              />
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
   );
