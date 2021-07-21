71c5f4607e3f1319e81329b53288a31c4f71bc65
irinamrdhtllh <irinamardhatillah@gmail.com>
2021-07-21 16:45:45 +0700

    Delete unused files

--- pages/test/level.js
+++
@@ -1,8 +0,0 @@
-import Level from '../../components/Level.js';
-import image from '../../public/images/image.jpg';
-
-export default function LevelTest() {
-  const level = "First Mate";
-
-  return <Level level_logo={image} level={level}/>;
-}
